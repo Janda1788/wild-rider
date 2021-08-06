d88f7dbb2fb0c7d92516732b90dcf2c843bc9b24
Janda1788 <35978133+Janda1788@users.noreply.github.com>
2021-08-06 12:25:35 -0600

    Que sirva

--- config/database.js
+++ config/database.js
@@ -1,39 +1,19 @@
-//module.exports = ({ env }) => ({
-//  defaultConnection: 'default',
-//  connections: {
-//    default: {
-//      connector: 'mongoose',
-//      settings: {
-//        uri: env('DATABASE_URI' || ''),
-//       host: env('DATABASE_HOST', '127.0.0.1'),
-//        srv: env.bool('DATABASE_SRV', false),
-//        srv: env.bool('DATABASE_SRV', true),
-//        port: env.int('DATABASE_PORT', 27017),
-//        database: env('DATABASE_NAME', ''),
-//      },
-//      options: {
-//       authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//        ssl: env.bool('DATABASE_SSL', true),
-//      },
-//    },
-//  },
-//})
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.jjnqd.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'wild-rider'),
-        username: env('DATABASE_USERNAME', 'WebMasterWild'),
-        password: env('DATABASE_PASSWORD', 'WildRider2021'),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABSE_PASSWORD'),
+        uri: env('DATABASE_URI')               
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+       authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', true),
       },
     },

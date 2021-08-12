6942e3f7524052ff915b443961cf13e69a8738bf
Janda1788 <35978133+Janda1788@users.noreply.github.com>
2021-08-11 20:48:24 -0600

    prueba

--- config/database.js
+++ config/database.js
@@ -4,16 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
+        uri: env('DATABASE_URI'),
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', ''),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABSE_PASSWORD'),
-        uri: env('DATABASE_URI')               
+        host: env('DATABASE_HOST', 'cluster0.jjnqd.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),        
+        database: env('DATABASE_NAME', 'wild-rider'),
+        username: env('DATABASE_USERNAME', 'WebMasterWild'),
+        password: env('DATABASE_PASSWORD', 'WildRider2021'),
       },
       options: {
-       authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', true),
       },
     },

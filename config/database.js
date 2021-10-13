598da972f011a1df33d9006f895da895aa7624a4
Janda1788 <35978133+Janda1788@users.noreply.github.com>
2021-10-12 21:50:03 -0600

    Database.js update

--- config/database.js
+++ config/database.js
@@ -6,8 +6,11 @@ module.exports = ({ env }) => ({
       settings: {
         uri: env("DATABASE_URI"),
         srv: env.bool("DATABASE_SRV", true),
+        host: env("DATABASE_HOST", "cluster0.jjnqd.mongodb.net"),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME"),
+        database: env("DATABASE_NAME", "wild-rider"),
+        username: env("DATABASE_USERNAME", "WebMasterWild"),
+        password: env("DATABASE_PASSWORD", "WildRider2021"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),

dbe63521edd0585cd8e3356a00a5ab5e4a63304c
Janda1788 <35978133+Janda1788@users.noreply.github.com>
2021-10-12 19:20:01 -0600

    Cambio en el database.js

--- config/database.js
+++ config/database.js
@@ -1,28 +1,18 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "mongoose",
-        settings: {
-          uri: env("DATABASE_URI"),
-          srv: env.bool("DATABASE_SRV", true),
-          host: env("DATABASE_HOST", "cluster0.jjnqd.mongodb.net"),
-          srv: env.bool("DATABASE_SRV", true),
-          port: env.int("DATABASE_PORT", 27017),
-          database: env("DATABASE_NAME", "wild-rider"),
-          username: env("DATABASE_USERNAME", "WebMasterWild"),
-          password: env("DATABASE_PASSWORD", "WildRider2021"),
-        },
-        options: {
-          authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-          ssl: env.bool("DATABASE_SSL", true),
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: env("DATABASE_URI"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME"),
+      },
+      options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
-  };
-};
+  },
+});
